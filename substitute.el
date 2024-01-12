1ac6d16a105030e34eca1ef09a8270fcadf86a4b
Protesilaos Stavrou <info@protesilaos.com>
2024-01-12 08:33:41 +0200

    Update copyright years

--- substitute.el
+++ substitute.el
@@ -1,6 +1,6 @@
 ;;; substitute.el --- Efficiently replace targets in the buffer or context -*- lexical-binding: t -*-
 
-;; Copyright (C) 2023  Free Software Foundation, Inc.
+;; Copyright (C) 2023-2024  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou General Issues <~protesilaos/general-issues@lists.sr.ht>
