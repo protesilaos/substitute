f4996f4391157989d7779fafc961dada89cec40e
Protesilaos Stavrou <info@protesilaos.com>
2024-01-17 05:00:48 +0200

    Update development links

--- substitute.el
+++ substitute.el
@@ -3,9 +3,8 @@
 ;; Copyright (C) 2023-2024  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
-;; Maintainer: Protesilaos Stavrou General Issues <~protesilaos/general-issues@lists.sr.ht>
-;; URL: https://git.sr.ht/~protesilaos/substitute
-;; Mailing-List: https://lists.sr.ht/~protesilaos/general-issues
+;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
+;; URL: https://github.com/protesilaos/substitute
 ;; Version: 0.2.1
 ;; Package-Requires: ((emacs "27.1"))
 
