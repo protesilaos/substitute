c46b2af332afa256e8f7c0a66b0f0530d7d30bbb
Protesilaos <info@protesilaos.com>
2026-07-28 18:46:18 +0300

    Revert "Simplify substitute--scope-current-and-above"
    This reverts commit 7c58550a1ce87921a3689e879dac410d0ce9903a.
    
    The change I made is not correct and thus introduces a bug.

--- substitute.el
+++ substitute.el
@@ -217,8 +217,9 @@ Pass to it the TARGET and SCOPE arguments."
 (defun substitute--scope-current-and-above (target)
   "Position point to match current TARGET and above."
   (substitute--widen)
-  (if (region-active-p)
-      (goto-char (region-beginning))
+  (if-let* ((_ (region-active-p))
+            (bounds (region-bounds)))
+      (goto-char (cdar bounds))
     (thing-at-point-looking-at target)
     (goto-char (match-end 0))))
 
